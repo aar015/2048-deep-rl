e821ca99306f1cc8ff311344c445d89aeeecf007
Avi Revah <avirlincoln@gmail.com>
2020-08-19 11:50:06 -0500

    Include CPU mode

--- run.sh
+++ run.sh
@@ -1,11 +1,23 @@
-#!/bin/sh
+#!/bin/bash
 
-docker run \
---rm \
---pid=host \
--p 9000:8888 \
--v "$PWD"/jupyter:/home \
--v "$PWD"/lab-config:/root/.jupyter/lab/user-settings \
--v "$PWD"/ssl:/ssl \
---gpus all \
-2048-jupyter:latest
+if command -v nvidia-smi &> /dev/null; then
+    echo "Hi"
+    docker run \
+    --rm \
+    --pid=host \
+    -p 9000:8888 \
+    -v "$PWD"/jupyter:/home \
+    -v "$PWD"/lab-config:/root/.jupyter/lab/user-settings \
+    -v "$PWD"/ssl:/ssl \
+    --gpus all \
+    2048-jupyter:latest
+else
+    docker run \
+    --rm \
+    --pid=host \
+    -p 9000:8888 \
+    -v "$PWD"/jupyter:/home \
+    -v "$PWD"/lab-config:/root/.jupyter/lab/user-settings \
+    -v "$PWD"/ssl:/ssl \
+    2048-jupyter:latest
+fi
